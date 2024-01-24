fb5d26ee3cdcf17e14bd7378994c92d04b08ba8a
Vikas Kumar <vikas_12@Vikazs-MacBook-Air.local>
2024-01-24 16:36:06 +0530

    first commit

---
+++ model/event.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your event product name!"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter your event product description!"],
+  },
+  start_Date: {
+    type: Date,
+    required: true,
+  },
+  Finish_Date: {
+    type: Date,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "Running",
+  },
+  tags: {
+    type: String,
+  },
+  images: [
+    {
+      type: String,
+    },
+  ],
+  shopId: {
+    type: String,
+    required: true,
+  },
+  shop: {
+    type: Object,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Event", eventSchema);
